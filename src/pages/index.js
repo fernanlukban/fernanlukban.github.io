ed7ef9e7a1f0907d7e1206da41ed07d8083354be
Fernan Lukban <fernanlukban@gmail.com>
2020-04-03 20:25:26 -0700

    added hr's for separators

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,34 +16,16 @@ const BlogIndex = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
+      <hr/>
       <Bio />
+      <hr/>
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
-          /*
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        */
-        <Card key={node.fields.slug} node={node}/>
+          <>
+            <Card key={node.fields.slug} node={node}/>
+            <hr/>
+          </>
         )
       })}
     </Layout>
